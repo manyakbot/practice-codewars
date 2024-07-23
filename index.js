30ae4a03e33ddbba00732f3108dafe84b8ed1c89
Manyakbot <skylinedelfly@gmail.com>
2024-07-23 09:25:16 +0500

    '08'

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function digPow(n, p) {
-    let digits = n.toString().split('').map(Number);
-    let sum = digits.reduce((acc, digit, index) => acc + Math.pow(digit, p + index), 0);
-    return sum % n === 0 ? sum / n : -1;
+function findEvenIndex(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let leftSum = arr.slice(0, i).reduce((acc, num) => acc + num, 0);
+        let rightSum = arr.slice(i + 1).reduce((acc, num) => acc + num, 0);
+        if (leftSum === rightSum) {
+            return i;
+        }
+    }
+    return -1;
 }
