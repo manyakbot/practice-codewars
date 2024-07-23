dd2d9fb15beb18a3e341accc7131cae6e23c9064
Manyakbot <skylinedelfly@gmail.com>
2024-07-23 09:22:17 +0500

    '05'

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-function tribonacci(signature, n) {
-    if (n === 0) return [];
-    if (n < 3) return signature.slice(0, n);
-    
-    let result = signature.slice();
-    
-    for (let i = 3; i < n; i++) {
-        result.push(result[i - 1] + result[i - 2] + result[i - 3]);
-    }
-    
-    return result;
+function solution(str) {
+  if (str.length % 2 !== 0) {
+    str += "_";
+  }
+
+  let result = [];
+  for (let i = 0; i < str.length; i += 2) {
+    result.push(str[i] + str[i + 1]);
+  }
+
+  return result;
 }
