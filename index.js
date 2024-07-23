4ae7cd2cbc33d3241c188b18582e174aaa8312f8
Manyakbot <skylinedelfly@gmail.com>
2024-07-23 09:26:34 +0500

    '09'

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-function findEvenIndex(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        let leftSum = arr.slice(0, i).reduce((acc, num) => acc + num, 0);
-        let rightSum = arr.slice(i + 1).reduce((acc, num) => acc + num, 0);
-        if (leftSum === rightSum) {
-            return i;
-        }
-    }
-    return -1;
+function decodeMorse(morseCode) {
+    morseCode = morseCode.trim();
+    
+    let words = morseCode.split('   ');
+    
+    let decodedWords = words.map(word => {
+        return word.split(' ').map(symbol => MORSE_CODE[symbol]).join('');
+    });
+    
+    return decodedWords.join(' ');
 }
